49a6f2c8c8f64459629d18e7b3e985960318d9e6
wonderfullss <dkrechin@list.ru>
2023-03-29 20:19:12 +0400

    #20

---
+++ src/Valid_Parentheses_20/Solution.java
@@ -0,0 +1,30 @@
+package Valid_Parentheses_20;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '(', '{', '[' -> stack.push(s.charAt(i));
+                case ')' -> {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                }
+                case '}' -> {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+                case ']' -> {
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
