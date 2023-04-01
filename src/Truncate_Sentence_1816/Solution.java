a44a5a66e4cde609f55c69ecb52054076dc2e4e1
wonderfullss <dkrechin@list.ru>
2023-04-01 20:39:52 +0400

    #1816

---
+++ src/Truncate_Sentence_1816/Solution.java
@@ -0,0 +1,13 @@
+package Truncate_Sentence_1816;
+
+public class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] arraySentence = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < k; i++){
+            result.append(arraySentence[i]);
+            result.append(" ");
+        }
+        return String.valueOf(result).trim();
+    }
+}
